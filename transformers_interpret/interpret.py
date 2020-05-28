09a24504b7c7abd75c2514ac30d66e92253e03d6
Charles Pierse <charlespierse@gmail.com>
2020-05-28 19:39:17 +0100

    Add signatures for explainer classes

--- transformers_interpret/interpret.py
+++ transformers_interpret/interpret.py
@@ -1,6 +1,24 @@
+import pandas
+from transformers import PreTrainedModel
 
 
 class BaseExplainer:
-    
-    def __init__(self):
-        pass 
+
+    def __init__(self, model: PreTrainedModel, model_type: str):
+        pass
+
+
+class SequenceClassificationExplainer(BaseExplainer):
+    pass
+
+
+class QuestionAnsweringExplainer(BaseExplainer):
+    pass
+
+
+class TokenClassificationExplainer(BaseExplainer):
+    pass
+
+
+class LMExplainer(BaseExplainer):
+    pass
