e98d916d55183bead9a8c78946d8099012aafe9b
Charles Pierse <charlespierse@gmail.com>
2021-05-11 17:12:13 +0100

    Make ZS explainer sublcass off sequence classification explainer

--- transformers_interpret/explainers/zero_shot_classification.py
+++ transformers_interpret/explainers/zero_shot_classification.py
@@ -1,6 +1,24 @@
-from transformers_interpret import SequenceClassificationExplainer
+from transformers import PreTrainedModel, PreTrainedTokenizer
+from transformers_interpret.errors import (
+    AttributionTypeNotSupportedError,
+    InputIdsNotCalculatedError,
+)
+
+from .sequence_classification import SequenceClassificationExplainer
+
+SUPPORTED_ATTRIBUTION_TYPES = ["lig"]
 
 
 class ZeroShotClassificationExplainer(SequenceClassificationExplainer):
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer,
+        attribution_type: str = "lig",
+    ):
+        super().__init__(model, tokenizer)
+        if attribution_type not in SUPPORTED_ATTRIBUTION_TYPES:
+            raise AttributionTypeNotSupportedError(
+                f"""Attribution type '{attribution_type}' is not supported.
+                Supported types are {SUPPORTED_ATTRIBUTION_TYPES}"""
+            )
