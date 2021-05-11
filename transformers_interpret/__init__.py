0d9159833276d68783585c61f970a795310c1174
Charles Pierse <charlespierse@gmail.com>
2021-05-11 17:11:40 +0100

    Update with new import structure

--- transformers_interpret/__init__.py
+++ transformers_interpret/__init__.py
@@ -2,3 +2,4 @@ from .attributions import Attributions, LIGAttributions
 from .explainer import BaseExplainer
 from .explainers.question_answering import QuestionAnsweringExplainer
 from .explainers.sequence_classification import SequenceClassificationExplainer
+from .explainers.zero_shot_classification import ZeroShotClassificationExplainer
