123b6526f262641a26559af66156939b7e246af2
Charles Pierse <charlespierse@gmail.com>
2021-05-12 11:20:02 +0100

    Added improved docstrings for classification explainer

--- transformers_interpret/explainers/sequence_classification.py
+++ transformers_interpret/explainers/sequence_classification.py
@@ -19,9 +19,8 @@ class SequenceClassificationExplainer(BaseExplainer):
     Explainer for explaining attributions for models of type
     `{MODEL_NAME}ForSequenceClassification` from the Transformers package.
 
-    Calculates attribution for `text` or `self.text` using the given model
-    and tokenizer. If `text` is not passed to the instantiated explainer
-    it is expected that text was passed in the constructor.
+    Calculates attribution for `text` using the given model
+    and tokenizer.
 
     Attributions can be forced along the axis of a particular output index or class name.
     To do this provide either a valid `index` for the class label's output or if the outputs
@@ -34,7 +33,7 @@ class SequenceClassificationExplainer(BaseExplainer):
     occur and the default word_embeddings will be chosen instead.
 
     Args:
-        text (str, optional): Text to provide attributions for. Defaults to None.
+        text (str): Text to provide attributions for.
         index (int, optional): Optional output index to provide attributions for. Defaults to None.
         class_name (str, optional): Optional output class name to provide attributions for. Defaults to None.
         embedding_type (int, optional): The embedding type word(0) or position(1) to calculate attributions for. Defaults to 0.
@@ -74,14 +73,17 @@ class SequenceClassificationExplainer(BaseExplainer):
 
         self._single_node_output = False
 
-    def encode(self, text: str = None) -> list:
+    def encode(self, text: str) -> list:
+        "Encode 'text' using tokenizer, special tokens are not added"
         return self.tokenizer.encode(text, add_special_tokens=False)
 
     def decode(self, input_ids: torch.Tensor) -> list:
+        "Decode 'input_ids' to string using tokenizer"
         return self.tokenizer.convert_ids_to_tokens(input_ids[0])
 
     @property
     def predicted_class_index(self) -> int:
+        "Returns predicted class index (int) for model with last calculated `input_ids`"
         if len(self.input_ids) > 0:
             # we call this before _forward() so it has to be calculated twice
             preds = self.model(self.input_ids)[0]
@@ -93,6 +95,7 @@ class SequenceClassificationExplainer(BaseExplainer):
 
     @property
     def predicted_class_name(self):
+        "Returns predicted class name (str) for model with last calculated `input_ids`"
         try:
             index = self.predicted_class_index
             return self.id2label[int(index)]
@@ -101,6 +104,7 @@ class SequenceClassificationExplainer(BaseExplainer):
 
     @property
     def word_attributions(self) -> list:
+        "Returns the word attributions for model and the text provided. Raises error if attributions not calculated."
         if self.attributions is not None:
             return self.attributions.word_attributions
         else:
@@ -109,6 +113,15 @@ class SequenceClassificationExplainer(BaseExplainer):
             )
 
     def visualize(self, html_filepath: str = None, true_class: str = None):
+        """
+        Visualizes word attributions. If in a notebook table will be displayed inline.
+
+        Otherwise pass a valid path to `html_filepath` and the visualization will be saved
+        as a html file.
+
+        If the true class is known for the text that can be passed to `true_class`
+        
+        """
         tokens = [token.replace("Ġ", "") for token in self.decode(self.input_ids)]
         attr_class = self.id2label[self.selected_index]
 
