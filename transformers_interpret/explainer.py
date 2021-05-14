7c640f1e71c67cd814c9e5a3021a70afad683a8e
Charles Pierse <charlespierse@gmail.com>
2021-05-14 17:06:47 +0100

    Formatting

--- transformers_interpret/explainer.py
+++ transformers_interpret/explainer.py
@@ -9,7 +9,12 @@ from transformers import PreTrainedModel, PreTrainedTokenizer
 
 
 class BaseExplainer(ABC):
-    def __init__(self, model: PreTrainedModel, tokenizer: PreTrainedTokenizer):
+    
+    def __init__(
+        self,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer,
+    ):
         self.model = model
         self.tokenizer = tokenizer
 
@@ -17,6 +22,7 @@ class BaseExplainer(ABC):
             self.ref_token_id = self.tokenizer.eos_token_id
         else:
             self.ref_token_id = self.tokenizer.pad_token_id
+
         self.sep_token_id = (
             self.tokenizer.sep_token_id
             if self.tokenizer.sep_token_id is not None
