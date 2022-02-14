ceda915aad7a6371d9c149780f49f284691a783d
Charles Pierse <charlespierse@gmail.com>
2022-02-14 13:43:59 +0000

    Update init with multiLabel

--- transformers_interpret/__init__.py
+++ transformers_interpret/__init__.py
@@ -3,3 +3,4 @@ from .explainer import BaseExplainer
 from .explainers.question_answering import QuestionAnsweringExplainer
 from .explainers.sequence_classification import SequenceClassificationExplainer
 from .explainers.zero_shot_classification import ZeroShotClassificationExplainer
+from .explainers.multilabel_classification import MultiLabelClassificationExplainer
