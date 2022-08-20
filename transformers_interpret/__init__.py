414137af68a3376e5a2dc678ce745165efd61f07
Charles Pierse <charlespierse@gmail.com>
2022-08-20 16:14:42 +0100

    Export pairwise explainer at project root

--- transformers_interpret/__init__.py
+++ transformers_interpret/__init__.py
@@ -1,7 +1,7 @@
 from .attributions import Attributions, LIGAttributions
 from .explainer import BaseExplainer
 from .explainers.question_answering import QuestionAnsweringExplainer
-from .explainers.sequence_classification import SequenceClassificationExplainer
+from .explainers.sequence_classification import SequenceClassificationExplainer, PairwiseSequenceClassificationExplainer
 from .explainers.zero_shot_classification import ZeroShotClassificationExplainer
 from .explainers.multilabel_classification import MultiLabelClassificationExplainer
 from .explainers.token_classification import TokenClassificationExplainer
