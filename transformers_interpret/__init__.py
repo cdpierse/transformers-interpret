176c273e2cbb4d99ba7f747fe0ab8b29588a3c01
Charles Pierse <charlespierse@gmail.com>
2022-09-04 17:23:22 +0100

    Formatting

--- transformers_interpret/__init__.py
+++ transformers_interpret/__init__.py
@@ -1,7 +1,10 @@
 from .attributions import Attributions, LIGAttributions
 from .explainer import BaseExplainer
 from .explainers.text.question_answering import QuestionAnsweringExplainer
-from .explainers.text.sequence_classification import SequenceClassificationExplainer, PairwiseSequenceClassificationExplainer
+from .explainers.text.sequence_classification import (
+    SequenceClassificationExplainer,
+    PairwiseSequenceClassificationExplainer,
+)
 from .explainers.text.zero_shot_classification import ZeroShotClassificationExplainer
 from .explainers.text.multilabel_classification import MultiLabelClassificationExplainer
 from .explainers.text.token_classification import TokenClassificationExplainer
