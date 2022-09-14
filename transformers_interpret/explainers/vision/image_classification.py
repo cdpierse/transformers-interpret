f5e4e08ffece22f1a34d48a82c6e4f00dc9ab457
Charles Pierse <charlespierse@gmail.com>
2022-09-14 22:36:37 +0100

    Add params to viz method

--- transformers_interpret/explainers/vision/image_classification.py
+++ transformers_interpret/explainers/vision/image_classification.py
@@ -1,5 +1,5 @@
 from enum import Enum, unique
-from typing import List, Optional, Tuple, Union
+from typing import Any, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -48,25 +48,33 @@ class ImageClassificationExplainer:
 
         self.internal_batch_size = None
         self.n_steps = 50
-        self.n_steps_noise_tunell = 5
-        self.noise_tunell_n_samples = 10
-        self.noise_tunell_type = NoiseTunnelType.SMOOTHGRAD.value
+        self.n_steps_noise_tunnel = 5
+        self.noise_tunnel_n_samples = 10
+        self.noise_tunnel_type = NoiseTunnelType.SMOOTHGRAD.value
 
         self.attributions = None
 
-    def visualize(self, save_path: str = None, show: bool = True, method: str = "overlay", sign: str = "all"):
-
+    def visualize(
+        self,
+        save_path: str = None,
+        method: str = "overlay",
+        sign: str = "all",
+        outlier_threshold: float = 0.1,
+    ):
+        outlier_threshold = min(outlier_threshold * 100, 100)
         np_attributions = np.transpose(self.attributions.squeeze().cpu().detach().numpy(), (1, 2, 0))
         np_image = np.transpose(self.inputs["pixel_values"].squeeze().cpu().detach().numpy(), (1, 2, 0))
         visualizer = ImageAttributionVisualizer(
             attributions=np_attributions,
             pixel_values=np_image,
-            outlier_threshold=self.outlier_threshold,
+            outlier_threshold=outlier_threshold,
             pred_class=self.id2label[self.predicted_index],
+            visualization_method=method,
         )
+        return visualizer()
 
-    def get_image(self):
-        return self.inputs["pixel_values"]
+    def image(self):
+        return self.visualize(method="original_image")
 
     def _forward_func(self, inputs):
         outputs = self.model(inputs)
@@ -96,10 +104,10 @@ class ImageClassificationExplainer:
             nt = NoiseTunnel(ig_nt)
             self.attributions = nt.attribute(
                 inputs["pixel_values"],
-                nt_samples=self.noise_tunell_n_samples,
-                nt_type=self.noise_tunell_type,
+                nt_samples=self.noise_tunnel_n_samples,
+                nt_type=self.noise_tunnel_type,
                 target=self.selected_index,
-                n_steps=self.n_steps_noise_tunell,
+                n_steps=self.n_steps_noise_tunnel,
             )
 
     def __call__(
@@ -109,24 +117,23 @@ class ImageClassificationExplainer:
         class_name: str = None,
         internal_batch_size: Union[int, None] = None,
         n_steps: Union[int, None] = None,
-        n_steps_noise_tunell: Union[int, None] = None,
-        noise_tunell_n_samples: Union[int, None] = None,
-        noise_tunell_type: NoiseTunnelType = NoiseTunnelType.SMOOTHGRAD,
+        n_steps_noise_tunnel: Union[int, None] = None,
+        noise_tunnel_n_samples: Union[int, None] = None,
+        noise_tunnel_type: NoiseTunnelType = NoiseTunnelType.SMOOTHGRAD,
         outlier_threshold: Union[float, None] = 0.1,
     ):
-        self.noise_tunell_type = noise_tunell_type.value
+        self.noise_tunnel_type = noise_tunnel_type.value
         self.inputs = self.feature_extractor(image, return_tensors="pt")
         self.predicted_index = self.model(self.inputs["pixel_values"]).logits.argmax().item()
-        self.outlier_threshold = outlier_threshold * 100
 
         if n_steps:
             self.n_steps = n_steps
-        if n_steps_noise_tunell:
-            self.n_steps_noise_tunell = n_steps_noise_tunell
+        if n_steps_noise_tunnel:
+            self.n_steps_noise_tunnel = n_steps_noise_tunnel
         if internal_batch_size:
             self.internal_batch_size = internal_batch_size
-        if noise_tunell_n_samples:
-            self.noise_tunell_n_samples = noise_tunell_n_samples
+        if noise_tunnel_n_samples:
+            self.noise_tunnel_n_samples = noise_tunnel_n_samples
 
         self._calculate_attributions(self.inputs, class_name, index)
 
@@ -154,15 +161,31 @@ class ImageAttributionVisualizer:
         pixel_values: np.ndarray,
         outlier_threshold: float,
         pred_class: str,
+        visualization_method: str,
     ):
         self.attributions = attributions
         self.pixel_values = pixel_values
         self.outlier_threshold = outlier_threshold
         self.pred_class = pred_class
-        self.render_pyplot = self._in_ipython()
+        self.render_pyplot = self._using_ipython()
+        try:
+            self.visualization_method = VisualizationMethods(visualization_method)
+        except ValueError:
+            raise ValueError(
+                f"""`visualization_method` must be one of the following: {list(VisualizationMethods.__members__.keys())}"""
+            )
+        if self.visualization_method == VisualizationMethods.HEATMAP:
+            self.plot_function = self.heatmap
+        elif self.visualization_method == VisualizationMethods.OVERLAY:
+            self.plot_function = self.overlay
+        elif self.visualization_method == VisualizationMethods.ALPHA_SCALING:
+            self.plot_function = self.alpha_scaling
+        elif self.visualization_method == VisualizationMethods.MASKED_IMAGE:
+            self.plot_function = self.masked_image
 
     def overlay(self):
         return viz.visualize_image_attr(
+            original_image=self.pixel_values,
             attr=self.attributions,
             sign="all",
             method="blended_heat_map",
@@ -174,6 +197,7 @@ class ImageAttributionVisualizer:
 
     def heatmap(self):
         return viz.visualize_image_attr(
+            original_image=self.pixel_values,
             attr=self.attributions,
             sign="all",
             method="heat_map",
@@ -188,6 +212,7 @@ class ImageAttributionVisualizer:
 
     def alpha_scaling(self):
         return viz.visualize_image_attr(
+            original_image=self.pixel_values,
             attr=self.attributions,
             sign="positive",
             method="alpha_scaling",
@@ -199,6 +224,7 @@ class ImageAttributionVisualizer:
 
     def masked_image(self):
         return viz.visualize_image_attr(
+            original_image=self.pixel_values,
             attr=self.attributions,
             sign="positive",
             method="masked_image",
@@ -220,10 +246,13 @@ class ImageAttributionVisualizer:
     def save(self):
         pass
 
-    def _in_ipython(self) -> bool:
+    def _using_ipython(self) -> bool:
         try:
             eval("__IPYTHON__")
         except NameError:
             return False
         else:  # pragma: no cover
             return True
+
+    def __call__(self):
+        return self.plot_function()
